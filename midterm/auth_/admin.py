ed0998511e840b60bed4606075e6a20e8e1431db
Ayana <ayananygmetova@gmail.com>
2021-03-20 12:39:48 +0600

    permisisons

--- midterm/auth_/admin.py
+++ midterm/auth_/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from auth_.models import MainUser
 
+
+@admin.register(MainUser)
+class MainUserAdmin(admin.ModelAdmin):
+    list_display = ['email',]
--- midterm/core/views.py
+++ midterm/core/views.py
@@ -13,10 +13,12 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerializer
 
     def get_permissions(self):
-        if (self.action=='destroy' or self.action=='update') and self.request.user.is_super_admin:
-            permission_classes= (IsAdminUser,)
-        else:
+        if (self.action=='destroy' or self.action=='update' or self.action=='create') and self.request.user.is_super_admin:
+            permission_classes= (AllowAny,)
+        elif (self.action=='list' or self.action=='retrieve') :
             permission_classes = (AllowAny,)
+        else:
+            permission_classes = (IsAdminUser,)
         return [permission() for permission in permission_classes]
 
 
@@ -25,3 +27,12 @@ class JournalViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = Journal.objects.all()
     serializer_class = JournalSerializer
+
+    def get_permissions(self):
+        if (self.action=='destroy' or self.action=='update' or self.action=='create') and self.request.user.is_super_admin:
+            permission_classes= (AllowAny,)
+        elif (self.action=='list' or self.action=='retrieve') :
+            permission_classes = (AllowAny,)
+        else:
+            permission_classes = (IsAdminUser,)
+        return [permission() for permission in permission_classes]
